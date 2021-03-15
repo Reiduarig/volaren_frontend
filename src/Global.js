f3af74a41c775fd93f3ef0525aeae5a981fb19b6
Reiduarig <70781686+Reiduarig@users.noreply.github.com>
2021-03-15 16:27:23 +0100

    Update Global.js

--- src/Global.js
+++ src/Global.js
@@ -1,4 +1,2 @@
 export const URL_API = "http://localhost:3999/api/";
 export const URL_CHAT = "http://localhost:3333";
-export const API_WEATHER = `http://api.openweathermap.org/data/2.5/weather`;
-export const API_KEY_WEATHER = "1c830a8e6eae69a99953287d7e76f942";
